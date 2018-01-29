eb516f4997444703b4c12cb2ab040f4f11b043b8
Rui Pereira <r4g3baby@gmail.com>
2018-01-29 23:28:24 +0000

    Added bStats Metrics

--- src/main/kotlin/com/r4g3baby/simplescore/SimpleScore.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/SimpleScore.kt
@@ -1,6 +1,7 @@
 package com.r4g3baby.simplescore
 
 import com.r4g3baby.simplescore.utils.updater.UpdateChecker
+import org.bstats.bukkit.MetricsLite
 import org.bukkit.plugin.java.JavaPlugin
 import java.util.function.Consumer
 
@@ -9,6 +10,7 @@ class SimpleScore : JavaPlugin() {
     override fun onEnable() {
         println("Enabling")
 
+        MetricsLite(this)
         UpdateChecker(this, 23243, Consumer {
             logger.warning("New version available download at:")
             logger.warning(it)
