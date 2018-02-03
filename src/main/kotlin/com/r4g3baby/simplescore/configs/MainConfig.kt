ad7fae2f69291d1a2193898992f320a793e22235
Rui Pereira <r4g3baby@gmail.com>
2018-02-03 01:01:17 +0000

    Finished main configuration

--- src/main/kotlin/com/r4g3baby/simplescore/configs/MainConfig.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/configs/MainConfig.kt
@@ -1,8 +1,40 @@
 package com.r4g3baby.simplescore.configs
 
 import com.r4g3baby.simplescore.SimpleScore
+import com.r4g3baby.simplescore.scoreboard.models.ScoreboardWorld
 import com.r4g3baby.simplescore.utils.configs.ConfigFile
+import java.util.*
+import kotlin.collections.HashMap
 
 class MainConfig(plugin: SimpleScore) : ConfigFile(plugin, "config") {
     val updateTime: Int = config.getInt("UpdateTime", 30)
+    val worlds: MutableMap<String, ScoreboardWorld> = HashMap()
+    val shared: MutableMap<String, List<String>> = HashMap()
+
+    init {
+        if (config.isConfigurationSection("Worlds")) {
+            val worldsSec = config.getConfigurationSection("Worlds")
+            for (world in worldsSec.getKeys(false).filter { plugin.server.getWorld(it) != null && !worlds.containsKey(it) }) {
+                if (worldsSec.isConfigurationSection(world)) {
+                    val worldSec = worldsSec.getConfigurationSection(world)
+                    val titles = LinkedList(worldSec.getStringList("Titles"))
+                    val scores = HashMap<Int, Queue<String>>()
+                    if (worldSec.isConfigurationSection("Scores")) {
+                        val scoresSec = worldSec.getConfigurationSection("Scores")
+                        for (score in scoresSec.getKeys(false).filter { !scores.containsKey(it.toInt()) }) {
+                            scores[score.toInt()] = LinkedList(scoresSec.getStringList(score))
+                        }
+                    }
+                    worlds[world] = ScoreboardWorld(titles, scores)
+                }
+            }
+        }
+
+        if (config.isConfigurationSection("Shared")) {
+            val sharedWorlds = config.getConfigurationSection("Shared")
+            for (world in sharedWorlds.getKeys(false).filter { worlds.containsKey(it) }) {
+                shared[world] = sharedWorlds.getStringList(world).filter { plugin.server.getWorld(it) != null }
+            }
+        }
+    }
 }
