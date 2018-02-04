12b0df874b3dda4df4fa02391c12e66420e06146
Rui Pereira <r4g3baby@gmail.com>
2018-02-04 00:47:39 +0000

    Changed reload permission

--- src/main/kotlin/com/r4g3baby/simplescore/commands/subcmds/Reload.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/commands/subcmds/Reload.kt
@@ -4,7 +4,7 @@ import com.r4g3baby.simplescore.SimpleScore
 import com.r4g3baby.simplescore.commands.SubCmd
 import org.bukkit.command.CommandSender
 
-class Reload(private val plugin: SimpleScore) : SubCmd("reload", "simplescore.admin") {
+class Reload(private val plugin: SimpleScore) : SubCmd("reload", "simplescore.reload") {
     override fun run(sender: CommandSender, args: List<String>) {
         sender.sendMessage(plugin.messagesConfig?.reloading)
         plugin.load()
