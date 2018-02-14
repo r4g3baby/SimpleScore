25da2ce08ccf170db693ceba645f61a5642873fa
Rui Pereira <r4g3baby@gmail.com>
2018-02-14 23:32:16 +0000

    Update checker now uses spigot api

--- src/main/kotlin/com/r4g3baby/simplescore/utils/updater/UpdateChecker.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/utils/updater/UpdateChecker.kt
@@ -1,9 +1,6 @@
 package com.r4g3baby.simplescore.utils.updater
 
 import org.bukkit.plugin.java.JavaPlugin
-import org.json.simple.JSONArray
-import org.json.simple.JSONObject
-import org.json.simple.JSONValue
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.net.HttpURLConnection
@@ -11,13 +8,13 @@ import java.net.URL
 import java.util.function.Consumer
 
 class UpdateChecker(plugin: JavaPlugin, pluginID: Int, consumer: Consumer<String?>) {
-    private val _spigetApi = "https://api.spiget.org/v2/resources/%s/versions?sort=-name"
+    private val _spigotApi = "https://api.spigotmc.org/legacy/update.php?resource=%s"
     private val _spigotUrl = "https://www.spigotmc.org/resources/%s"
 
     init {
         plugin.server.scheduler.runTaskAsynchronously(plugin, {
             try {
-                val conn = URL(_spigetApi.format(pluginID)).openConnection() as HttpURLConnection
+                val conn = URL(_spigotApi.format(pluginID)).openConnection() as HttpURLConnection
                 conn.requestMethod = "GET"
                 conn.useCaches = false
                 conn.connectTimeout = 3000
@@ -25,13 +22,15 @@ class UpdateChecker(plugin: JavaPlugin, pluginID: Int, consumer: Consumer<String
                 conn.setRequestProperty("Accept", "application/json")
                 conn.setRequestProperty("User-Agent", "${plugin.name}/${plugin.description.version}")
 
-                val versions = JSONValue.parseWithException(BufferedReader(InputStreamReader(conn.inputStream))) as JSONArray
-                val latestVersion = ((versions[0] as JSONObject)["name"] as String).replace(".", "")
+                val reader = BufferedReader(InputStreamReader(conn.inputStream))
+                val latestVersion = reader.readText().replace(".", "")
                 val currentVersion = plugin.description.version.replace(".", "")
 
                 if (currentVersion.toInt() < latestVersion.toInt()) {
                     consumer.accept(_spigotUrl.format(pluginID))
                 }
+
+                reader.close()
                 conn.disconnect()
             } catch (ignored: Exception) {
             }
