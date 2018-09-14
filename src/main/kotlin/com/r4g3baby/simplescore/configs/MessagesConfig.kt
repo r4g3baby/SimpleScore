6d7648278b6a57667f06e8effd0edab0c3f4dcfa
Rui Pereira <r4g3baby@gmail.com>
2018-09-14 10:35:12 +0100

    Added a prefix to all plugin messages

--- src/main/kotlin/com/r4g3baby/simplescore/configs/MessagesConfig.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/configs/MessagesConfig.kt
@@ -6,12 +6,14 @@ import org.bukkit.ChatColor
 
 class MessagesConfig(plugin: SimpleScore) : ConfigFile(plugin, "messages") {
     val help = color(config.getString("Help", "&cN/A"))
-    val permission = color(config.getString("Permission", "&cN/A"))
-    val onlyPlayers = color(config.getString("OnlyPlayers", "&cN/A"))
-    val reloading = color(config.getString("Reloading", "&cN/A"))
-    val reloaded = color(config.getString("Reloaded", "&cN/A"))
-    val enabled = color(config.getString("Enabled", "&cN/A"))
-    val disabled = color(config.getString("Disabled", "&cN/A"))
+
+    private val prefix = color(config.getString("Prefix", "&8[&bSimpleScore&8] "))
+    val permission = prefix + color(config.getString("Permission", "&cN/A"))
+    val onlyPlayers = prefix + color(config.getString("OnlyPlayers", "&cN/A"))
+    val reloading = prefix + color(config.getString("Reloading", "&cN/A"))
+    val reloaded = prefix + color(config.getString("Reloaded", "&cN/A"))
+    val enabled = prefix + color(config.getString("Enabled", "&cN/A"))
+    val disabled = prefix + color(config.getString("Disabled", "&cN/A"))
 
     private fun color(text: String): String = ChatColor.translateAlternateColorCodes('&', text)
 }
