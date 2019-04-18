8313f4d485c4f701385d15bf2f405e60247b2bf7
Rui Pereira <r4g3baby@gmail.com>
2019-04-18 23:48:32 +0100

    Redid the ScoreLine class to address some issues

--- src/main/kotlin/com/r4g3baby/simplescore/scoreboard/models/ScoreLine.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/scoreboard/models/ScoreLine.kt
@@ -1,31 +1,33 @@
 package com.r4g3baby.simplescore.scoreboard.models
 
-import java.util.*
-
 class ScoreLine {
-    private var lines: Queue<String> = LinkedList()
-    private var timings: Queue<Int> = LinkedList()
+    private var lines: MutableList<String> = ArrayList()
+    private var timings: MutableList<Int> = ArrayList()
 
     fun add(text: String, time: Int) {
         lines.add(text)
         timings.add(time)
     }
 
+    private var current: Int = 0
     private var frames: Int = 0
     fun nextFrame(): String {
-        if (frames >= timings.peek()) {
-            lines.add(lines.poll())
-            timings.add(timings.poll())
+        if (frames >= timings[current]) {
+            current++
+            if (current >= lines.size) {
+                current = 0
+            }
             frames = 0
         }
         frames++
-        return lines.peek()
+        return lines[current]
     }
 
     fun clone(): ScoreLine {
         val clone = ScoreLine()
-        clone.lines = LinkedList(lines)
-        clone.timings = LinkedList(timings)
+        clone.lines = ArrayList(lines)
+        clone.timings = ArrayList(timings)
+        clone.current = current
         clone.frames = frames
         return clone
     }
