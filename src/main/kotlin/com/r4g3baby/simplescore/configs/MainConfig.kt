87d11d154d346feb87c8c2b5e3fdf63f4d06bce8
Rui Pereira <me@ruipereira.dev>
2021-08-14 23:55:53 +0100

    Initial work on config parser

--- src/main/kotlin/com/r4g3baby/simplescore/configs/MainConfig.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/configs/MainConfig.kt
@@ -1,94 +1,54 @@
 package com.r4g3baby.simplescore.configs
 
 import com.r4g3baby.simplescore.SimpleScore
-import com.r4g3baby.simplescore.scoreboard.models.ScoreLine
 import com.r4g3baby.simplescore.scoreboard.models.Scoreboard
 import com.r4g3baby.simplescore.utils.configs.ConfigFile
 import java.util.function.Predicate
 import java.util.regex.Pattern
-import kotlin.collections.HashMap
 
-class MainConfig(plugin: SimpleScore) : ConfigFile(plugin, "config") {
-    private val updateTime = config.getInt("UpdateTime", 20)
-    val saveScoreboards = config.getBoolean("SaveScoreboards", true)
-    val asyncPlaceholders = config.getBoolean("AsyncPlaceholders", true)
-    val forceLegacy = config.getBoolean("ForceLegacy", false)
+class MainConfig(plugin: SimpleScore, fileName: String = "config") : ConfigFile(plugin, fileName) {
+    private val version = config.getInt("version", -1)
+    private val updateTime = config.getInt("updateTime", 20)
+    val saveScoreboards = config.getBoolean("saveScoreboards", true)
+    val asyncPlaceholders = config.getBoolean("asyncPlaceholders", true)
+    val forceLegacy = config.getBoolean("forceLegacy", false)
 
     val scoreboards = HashMap<String, Scoreboard>()
     val worlds = HashMap<Predicate<String>, List<String>>()
 
     init {
-        // Compatibility with older config format
-        if (config.isConfigurationSection("Worlds") && !config.isConfigurationSection("Scoreboards")) {
-            config.createSection("Scoreboards", config.getConfigurationSection("Worlds").getValues(true))
-            config.set("Worlds", null)
+        if (version < 0) {
+            // Compatibility with older config format
+            if (config.isConfigurationSection("Worlds") && !config.isConfigurationSection("Scoreboards")) {
+                config.createSection("Scoreboards", config.getConfigurationSection("Worlds").getValues(true))
+                config.set("Worlds", null)
+            }
+
+            if (config.isConfigurationSection("Shared") && !config.isConfigurationSection("Worlds")) {
+                val sharedSec = config.getConfigurationSection("Shared")
+                val worldsSec = config.createSection("Worlds")
+
+                if (config.isConfigurationSection("Scoreboards")) {
+                    for (scoreboard in config.getConfigurationSection("Scoreboards").getKeys(false)) {
+                        worldsSec.set(scoreboard, scoreboard)
+                    }
+                }
+
+                for (shared in sharedSec.getKeys(false)) {
+                    for (world in sharedSec.getStringList(shared)) {
+                        worldsSec.set(world, shared)
+                    }
+                }
+
+                config.set("Shared", null)
+            }
+
+            config.set("version", 1)
             config.save(this)
         }
 
-        if (config.isConfigurationSection("Shared") && !config.isConfigurationSection("Worlds")) {
-            val sharedSec = config.getConfigurationSection("Shared")
-            val worldsSec = config.createSection("Worlds")
-
-            if (config.isConfigurationSection("Scoreboards")) {
-                for (scoreboard in config.getConfigurationSection("Scoreboards").getKeys(false)) {
-                    worldsSec.set(scoreboard, scoreboard)
-                }
-            }
-
-            for (shared in sharedSec.getKeys(false)) {
-                for (world in sharedSec.getStringList(shared)) {
-                    worldsSec.set(world, shared)
-                }
-            }
-
-            config.set("Shared", null)
-            config.save(this)
-        }
-
-        if (config.isConfigurationSection("Scoreboards")) {
-            val scoreboardsSec = config.getConfigurationSection("Scoreboards")
-            for (scoreboard in scoreboardsSec.getKeys(false).filter { !scoreboards.containsKey(it.lowercase()) }) {
-                if (scoreboardsSec.isConfigurationSection(scoreboard)) {
-                    val scoreboardSec = scoreboardsSec.getConfigurationSection(scoreboard)
-
-                    val titles = ScoreLine()
-                    scoreboardSec.getList("Titles").forEach {
-                        when (it) {
-                            is String -> titles.add(it, updateTime)
-                            is Map<*, *> -> titles.add(it["text"] as String, it["time"] as Int)
-                            else -> {
-                                plugin.logger.warning("Failed to parse titles expected String or Map but got ${it!!::class.java} instead.")
-                            }
-                        }
-                    }
-
-                    val scores = HashMap<Int, ScoreLine>()
-                    if (scoreboardSec.isConfigurationSection("Scores")) {
-                        val scoresSec = scoreboardSec.getConfigurationSection("Scores")
-                        for (score in scoresSec.getKeys(false).filter { !scores.containsKey(it.toInt()) }) {
-                            val scoreLine = ScoreLine()
-                            scoresSec.getList(score).forEach {
-                                when (it) {
-                                    is String -> scoreLine.add(it, updateTime)
-                                    is Map<*, *> -> scoreLine.add(it["text"] as String, it["time"] as Int)
-                                    else -> {
-                                        plugin.logger.warning("Failed to parse score \"$score\" expected String or Map but got ${it!!::class.java} instead.")
-                                    }
-                                }
-                            }
-                            scores[score.toInt()] = scoreLine
-                        }
-                    }
-
-                    val name = scoreboard.lowercase()
-                    val restricted = scoreboardSec.getBoolean("Restricted", false)
-                    scoreboards[name] = Scoreboard(name, titles, scores, restricted)
-                }
-            }
-        }
-
-        if (config.isConfigurationSection("Worlds")) {
-            val worldsSec = config.getConfigurationSection("Worlds")
+        if (config.isConfigurationSection("worlds")) {
+            val worldsSec = config.getConfigurationSection("worlds")
             for (world in worldsSec.getKeys(false)) {
                 val pattern = Pattern.compile("^${world}$", Pattern.CASE_INSENSITIVE)
                 val scoreboards = worldsSec.get(world)
@@ -103,7 +63,6 @@ class MainConfig(plugin: SimpleScore) : ConfigFile(plugin, "config") {
                     is String -> listOf(scoreboards.lowercase())
                     else -> emptyList()
                 }
-
             }
         }
     }
