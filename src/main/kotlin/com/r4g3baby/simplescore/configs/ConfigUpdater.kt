f0b6b81fd71d78c3d7a82cefdf303023c79ca491
Rui Pereira <me@ruipereira.dev>
2022-02-17 23:49:24 +0000

    Add support for automatic updating to new config format

--- src/main/kotlin/com/r4g3baby/simplescore/configs/ConfigUpdater.kt
+++ src/main/kotlin/com/r4g3baby/simplescore/configs/ConfigUpdater.kt
@@ -14,78 +14,127 @@ class ConfigUpdater(plugin: Plugin) {
     private val conditionsConfigFile = ConfigFile(plugin, "conditions")
     private val conditionsConfig = conditionsConfigFile.config
 
+    private var version = mainConfig.getInt("version", -1)
+
     init {
-        val version = mainConfig.getInt("version", -1)
-        if (version < 0) {
-            plugin.logger.info("Detected an old config format, the plugin will create a backup and attempt to update it.")
-            mainConfigFile.copyTo(File(mainConfigFile.parentFile, "config.bak"), true)
-
-            mainConfig.set("language", "en")
-            mainConfig.set("checkForUpdates", true)
-
-            val updateTime = mainConfig.getInt("UpdateTime", 20)
-            if (mainConfig.contains("UpdateTime")) {
-                mainConfig.set("UpdateTime", null)
+        var firstRun = true
+        var updateFunc = getUpdateFunc()
+        while (updateFunc != null) {
+            if (firstRun) {
+                plugin.logger.info("Detected an old config format (v$version), the plugin will create a backup and attempt to update it.")
+                mainConfigFile.copyTo(File(mainConfigFile.parentFile, "config.bak"), true)
+                firstRun = false
             }
 
-            if (mainConfig.contains("SaveScoreboards")) {
-                mainConfig.set("savePlayerData", mainConfig.get("SaveScoreboards"))
-                mainConfig.set("SaveScoreboards", null)
-            }
+            version = updateFunc()
 
-            if (mainConfig.contains("AsyncPlaceholders")) {
-                mainConfig.set("asyncPlaceholders", mainConfig.get("AsyncPlaceholders"))
-                mainConfig.set("AsyncPlaceholders", null)
-            }
+            mainConfig.set("version", version)
+            mainConfig.save(mainConfigFile)
 
-            if (mainConfig.contains("ForceLegacy")) {
-                mainConfig.set("forceLegacy", mainConfig.get("ForceLegacy"))
-                mainConfig.set("ForceLegacy", null)
-            }
+            plugin.logger.info("Config updated to version $version.")
 
-            if (mainConfig.isConfigurationSection("Scoreboards")) {
-                val scoreboardsSection = mainConfig.getConfigurationSection("Scoreboards")
-                for (scoreboard in scoreboardsSection.getKeys(false)) {
-                    scoreboardsConfig.set(scoreboard, scoreboardsSection.getConfigurationSection(scoreboard))
+            updateFunc = getUpdateFunc()
+        }
+    }
 
-                    scoreboardsConfig.getConfigurationSection(scoreboard).apply {
-                        if (updateTime != 20) {
-                            set("updateTime", updateTime)
-                        }
+    private fun getUpdateFunc(): (() -> Int)? {
+        return when (version) {
+            -1, 0 -> return version@{
+                mainConfig.set("language", "en")
+                mainConfig.set("checkForUpdates", true)
 
-                        if (getBoolean("Restricted", false)) {
-                            val condition = "sb$scoreboard"
-                            conditionsConfig.createSection(
-                                condition, mapOf(
-                                    "type" to "HAS_PERMISSION",
-                                    "permission" to "simplescore.${scoreboard.lowercase()}"
-                                )
-                            )
-
-                            set("conditions", listOf(condition))
-                            set("Restricted", null)
-                        }
-
-                        set("titles", get("Titles"))
-                        set("Titles", null)
-
-                        set("scores", get("Scores"))
-                        set("Scores", null)
-                    }
+                val updateTime = mainConfig.getInt("UpdateTime", 20)
+                if (mainConfig.contains("UpdateTime")) {
+                    mainConfig.set("UpdateTime", null)
                 }
 
-                mainConfig.set("Scoreboards", null)
-                conditionsConfig.save(conditionsConfigFile)
-                scoreboardsConfig.save(scoreboardsConfigFile)
-            }
+                if (mainConfig.contains("SaveScoreboards")) {
+                    mainConfig.set("savePlayerData", mainConfig.get("SaveScoreboards"))
+                    mainConfig.set("SaveScoreboards", null)
+                }
 
-            if (mainConfig.isConfigurationSection("Worlds")) {
-                mainConfig.set("worlds", mainConfig.getConfigurationSection("Worlds"))
-                mainConfig.set("Worlds", null)
-            }
+                if (mainConfig.contains("AsyncPlaceholders")) {
+                    mainConfig.set("asyncPlaceholders", mainConfig.get("AsyncPlaceholders"))
+                    mainConfig.set("AsyncPlaceholders", null)
+                }
 
-            mainConfig.set("version", 1)
-            mainConfig.save(mainConfigFile)
+                if (mainConfig.contains("ForceLegacy")) {
+                    mainConfig.set("forceLegacy", mainConfig.get("ForceLegacy"))
+                    mainConfig.set("ForceLegacy", null)
+                }
+
+                if (mainConfig.isConfigurationSection("Scoreboards")) {
+                    val scoreboardsSection = mainConfig.getConfigurationSection("Scoreboards")
+                    for (scoreboard in scoreboardsSection.getKeys(false)) {
+                        scoreboardsConfig.set(scoreboard, scoreboardsSection.getConfigurationSection(scoreboard))
+
+                        scoreboardsConfig.getConfigurationSection(scoreboard).apply {
+                            if (updateTime != 20) {
+                                set("updateTime", updateTime)
+                            }
+
+                            if (getBoolean("Restricted", false)) {
+                                val condition = "sb$scoreboard"
+                                conditionsConfig.createSection(
+                                    condition, mapOf(
+                                        "type" to "HAS_PERMISSION",
+                                        "permission" to "simplescore.${scoreboard.lowercase()}"
+                                    )
+                                )
+
+                                set("conditions", listOf(condition))
+                                set("Restricted", null)
+                            }
+
+                            set("titles", get("Titles"))
+                            set("Titles", null)
+
+                            set("scores", get("Scores"))
+                            set("Scores", null)
+                        }
+                    }
+
+                    mainConfig.set("Scoreboards", null)
+                    conditionsConfig.save(conditionsConfigFile)
+                    scoreboardsConfig.save(scoreboardsConfigFile)
+                }
+
+                if (mainConfig.isConfigurationSection("Worlds")) {
+                    mainConfig.set("worlds", mainConfig.getConfigurationSection("Worlds"))
+                    mainConfig.set("Worlds", null)
+                }
+
+                return@version 1
+            }
+            1 -> return version@{
+                val storageDriver = if (mainConfig.getBoolean("savePlayerData", false)) "h2" else "none"
+                mainConfig.set("savePlayerData", null)
+
+                mainConfig.createSection(
+                    "storage", mapOf(
+                        "driver" to storageDriver,
+                        "tablePrefix" to "simplescore_",
+                        "address" to "127.0.0.1:3306",
+                        "database" to "minecraft",
+                        "username" to "simplescore",
+                        "password" to "|D/-\\55\\^/0|2|)",
+                        "pool" to mapOf(
+                            "maximumPoolSize" to 8,
+                            "minimumIdle" to 8,
+                            "maxLifetime" to 1800000,
+                            "keepaliveTime" to 0,
+                            "connectionTimeout" to 5000,
+                            "extraProperties" to mapOf(
+                                "useUnicode" to true,
+                                "characterEncoding" to "utf8"
+                            )
+                        )
+                    )
+                )
+
+                return@version 2
+            }
+            else -> null
         }
     }
 }
