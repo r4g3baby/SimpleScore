9f47dfdeecba659a3a6df4f48870b462fbecec51
Rui Pereira <me@ruipereira.dev>
2023-10-12 17:47:44 +0100

    Bump ShadowJar version from 7.1.2 to 8.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,5 @@
 plugins {
-    id("com.github.johnrengelman.shadow") version "7.1.2"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm") version "1.9.10"
     id("maven-publish")
 }
